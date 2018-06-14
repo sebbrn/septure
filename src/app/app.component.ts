fcefd0041ada10dc51f68b2d055fc66673f39e88
Sebastian Brune <s.brun@gmx.de>
2018-06-14 22:31:29 +0200

    added smooth scrolling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  scrollToElement(selector: string) {
+    document.querySelector(selector).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  }
 }
